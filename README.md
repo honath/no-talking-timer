21ab68438bc45ed1a3544d25c26272ebff6534ff
Alexander Cooper <thechosenwaffle@gmail.com>
2021-06-04 00:25:50 -0400

    Initial commit. Includes functional timer and button toggling.

---
+++ README.md
@@ -0,0 +1,90 @@
+# 5 Minute "Hush" Countdown Timer for OBS
+
+## Overview
+
+This is a basic timer to be used as an OBS Browser Source - it is set to a default time of 5 minutes,
+and for the duration of the time, a timer and an image is displayed. When the timer ends,
+the timer and image is hidden again.
+
+## Usage
+
+Feel free to add this as a browser source and use it inside of your own streams.
+
+You can change the configuration by forking and cloning this repository -
+change the "MINUTES" variable in the `script.js` file to the amount of minutes of your choosing,
+and then host the document on whichever hosting service you prefer. I am using GitHub.
+
+You can view how to add a browser source to OBS
+[here.](https://blog.streamlabs.com/introducing-browser-source-interaction-for-streamlabs-obs-d8fc4dcbb1fb)
+
+### Recommended OBS Window Settings for Browser Source
+
+- Width: 500
+- Height: 1080
+- This was designed to be placed over a webcam at the bottom of the screen, so that the button is hidden off-screen.
+
+### Tips for Using this Browser Source
+
+- You do not need to have the actual web page open in a tab - instead, `right click` on the `Browser Source` **inside** of your `Scene`.
+  Then, click `Interact` and OBS will open a new, small window with the `'Start'` button visible and clickable.
+  You can use this instead of an open window or tab in your browser, and is my recommendation.
+- Feel free to use custom CSS in the source properties. The image and timer are inside of an element with a class of `.wrapper` - you
+  should be able to select it from there, and move it around if needed.
+- Learn more about CSS at [W3Schools.com](https://www.w3schools.com/css/default.asp)
+
+### Additional Customization within OBS
+
+##### You can change the font for all elements using custom CSS. Here is the styling for the font + its selector:
+
+```
+* {
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  color: #fff;
+  -webkit-text-stroke: 2px black;
+}
+```
+
+##### Here are the "Start" button's CSS properties + its selector, for your custom CSS use:
+
+```
+#start {
+  font-size: 5rem;
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  left: 50%;
+  margin-left: -250px;
+  top: 50%;
+  color: #000;
+  border: none;
+}
+```
+
+## File Structure
+
+- ./ (Root folder)
+  - index.html (Document structure)
+  - style.css (Styling for entire document)
+  - script.js (Manages display of the primary element and function of the timer)
+  - ./images/ (Folder for containing any images)
+    - shush.jpg (Image used in browser source)
+  - README.md (This README document)
+
+## Changelog
+
+### 6/4/2021 - v1.0
+
+- Initial release
+- Features 5 minute timer
+- Timer is hidden initially - is visible after "Start" button is pressed, until the timer ends
+
+## Planned Improvements
+
+- Add a "Pause" button
+- Add a "Stop" button
+- Make buttons prettier
+- Make "hush" image danker
+
+## Bugs and Suggestions
+
+Please report any bugs as an `issue` here on GitHub - suggestions are also welcome.
---
+++ script.js
@@ -0,0 +1,159 @@
+/**
+ * Get the primary element ".wrapper" from the page
+ * Hide the element on load, to be toggled in later use
+ */
+const browserDisplay = document.querySelector(".wrapper");
+browserDisplay.style.display = "none";
+
+/* Grab timer element from page */
+const displayTime = document.querySelector("#time-remaining");
+
+/* "Click" event listener for "Start" button - initializes timer */
+document.querySelector("#start").addEventListener("click", startClickHandler);
+
+/* Flag to determine if timer is already active */
+let isActiveFlag = false;
+
+/* Function to toggle the "is active" flag */
+const toggleActiveFlag = () => {
+  isActiveFlag = !isActiveFlag;
+};
+
+/**
+ * Array object to store all references of setInterval timers
+ * This is being done because if the "start" button is
+ * pressed multiple times, the timer on display
+ * will begin to flick between active timers,
+ * and so they must be cleared
+ */
+let intervalsArray = [];
+
+/* Function for clearing active timers */
+const clearAllTimers = (timer) => {
+  clearInterval(timer);
+  intervalsArray = [];
+};
+
+/**
+ * On "start" button click,
+ * initialize timer and display
+ * the primary element
+ */
+function startClickHandler() {
+  /* Clear all active timers when function starts */
+  intervalsArray.map(clearAllTimers);
+
+  /**
+   * Check if the function is already
+   * active - if it is, reset the
+   * "active" flag and call again
+   * recursively
+   * ===========================
+   * Otherwise, simply toggle the active flag
+   * to "true" and continue
+   */
+  if (isActiveFlag === true) {
+    toggleActiveFlag();
+
+    /* Recursive call */
+    startClickHandler();
+  } else {
+    toggleActiveFlag();
+  }
+
+  /* {CONFIG} Number of minutes for the timer to run */
+  const MINUTES = 5;
+
+  /* Make primary element visible */
+  visibilityToggler();
+
+  /* Convert minutes to seconds for timer calculations */
+  let timeInSeconds = 60 * MINUTES;
+
+  /**
+   * Initialize the timer so it appears instantly
+   * Display the time
+   * Decrement by one
+   */
+  const initializeTimer = () => {
+    /* Set initial time as a formatted string */
+    const formattedTime = formatTime(timeInSeconds);
+
+    /* Update the text of the timer on the page */
+    displayTime.innerText = formattedTime;
+
+    /* Decrement the timer by 1 */
+    timeInSeconds--;
+  };
+
+  initializeTimer();
+
+  /**
+   * Push new timer interval into
+   * array object
+   * ===============================
+   * Every 1 second (1000ms), update
+   * the timer on the page
+   */
+  intervalsArray.push(
+    setInterval(() => {
+      /* Set time as a formatted string */
+      const formattedTime = formatTime(timeInSeconds);
+
+      /**
+       * Check if the timer has finished
+       * If it has, hide the primary element
+       * and clear the repeating interval
+       */
+      if (timeInSeconds < 0) {
+        visibilityToggler();
+        clearInterval(window.countdownTimer);
+
+        toggleActiveFlag();
+      }
+
+      /* Update the text of the timer on the page */
+      displayTime.innerText = formattedTime;
+
+      /* Decrement the timer by 1 */
+      timeInSeconds--;
+    }, 1000)
+  );
+}
+
+/**
+ * Toggles the visibility/display
+ * of the primary element (.wrapper)
+ * between "block" and "none"
+ */
+function visibilityToggler() {
+  /* If "none", change to "block", and vice versa */
+  if (browserDisplay.style.display === "none")
+    browserDisplay.style.display = "block";
+  else browserDisplay.style.display = "none";
+}
+
+/**
+ * Takes in time remaining in seconds
+ * and formats the timer text to be
+ * displayed on the web page
+ * @param {Integer} seconds
+ * @returns {String}
+ */
+function formatTime(seconds) {
+  /* Calculate minutes and seconds remaining in timer */
+  const minRemaining = Math.floor(seconds / 60);
+  const secRemaining = seconds % 60;
+
+  /**
+   * Set time as a formatted string,
+   * change display to include an extra 0
+   * if the minutes are less than 10
+   */
+  const formattedTime =
+    secRemaining < 10
+      ? `${minRemaining}:0${secRemaining}`
+      : `${minRemaining}:${secRemaining}`;
+
+  return formattedTime;
+}
